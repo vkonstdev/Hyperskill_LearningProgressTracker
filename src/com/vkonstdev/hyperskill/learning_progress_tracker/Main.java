0e49ba31c66196854658e47d3d7c8cf13cbf4107
vkonstdev <vkonstdev@gmail.com>
2023-02-07 17:55:48 +0700

    Stage 1/4 complete

--- src/com/vkonstdev/hyperskill/learning_progress_tracker/Main.java
+++ src/com/vkonstdev/hyperskill/learning_progress_tracker/Main.java
@@ -1,8 +1,22 @@
 package com.vkonstdev.hyperskill.learning_progress_tracker;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Learning Progress Tracker");
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String in = sc.nextLine();
+            if (in.isEmpty() || in.isBlank()) {
+                System.out.println("No input.");
+            } else if ("exit".equals(in)) {
+                System.out.println("Bye!");
+                break;
+            } else {
+                System.out.println("Error: unknown command!");
+            }
+        }
     }
 }
